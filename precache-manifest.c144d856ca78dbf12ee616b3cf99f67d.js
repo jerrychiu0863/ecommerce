4d27cb2ff3e796923f287d13ee8f53a9f9fa0a1b
Jerry <m08632000@hotmail.com>
2019-09-09 11:38:42 +0800

    Updates

---
+++ precache-manifest.c144d856ca78dbf12ee616b3cf99f67d.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a36f59ddb4d2d11317eb89c9a8745a4",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "aa6716110b929367e694",
+    "url": "/ecommerce/static/css/2.1a17ec54.chunk.css"
+  },
+  {
+    "revision": "9c0eb1f79b19702dbc0d",
+    "url": "/ecommerce/static/css/main.1ca2f6e3.chunk.css"
+  },
+  {
+    "revision": "aa6716110b929367e694",
+    "url": "/ecommerce/static/js/2.6b5a594d.chunk.js"
+  },
+  {
+    "revision": "9c0eb1f79b19702dbc0d",
+    "url": "/ecommerce/static/js/main.b150a399.chunk.js"
+  },
+  {
+    "revision": "9edc88e09ae1f38c691c",
+    "url": "/ecommerce/static/js/runtime~main.83927a81.js"
+  },
+  {
+    "revision": "02ede1c1b0a32ba1822d7e5c0cdc2585",
+    "url": "/ecommerce/static/media/acrylic-earing_720x480.02ede1c1.jpg"
+  },
+  {
+    "revision": "31835d46c1e470e59aa51e562c4f26f6",
+    "url": "/ecommerce/static/media/beautiful-bracelet_720x480.31835d46.jpg"
+  },
+  {
+    "revision": "bfb92e220581827fc6267218112f3428",
+    "url": "/ecommerce/static/media/carousel_four_medium_720x480.bfb92e22.jpg"
+  },
+  {
+    "revision": "355f67f374b2a7bd243e47ae09c7de7c",
+    "url": "/ecommerce/static/media/carousel_one_medium_720x480.355f67f3.jpg"
+  },
+  {
+    "revision": "6478c319d353de71c4b5afcb9764a79d",
+    "url": "/ecommerce/static/media/carousel_three_medium_720x480.6478c319.jpg"
+  },
+  {
+    "revision": "43db020dda4fe38c14375b2ffad339bc",
+    "url": "/ecommerce/static/media/carousel_two_medium_720x480.43db020d.jpg"
+  },
+  {
+    "revision": "c30a6b1e0192c353316fc0541d1854ee",
+    "url": "/ecommerce/static/media/couple-bracelet_720x480.c30a6b1e.jpg"
+  },
+  {
+    "revision": "c5405f9dfd70091ab184eb26de523900",
+    "url": "/ecommerce/static/media/couple-rings_720x480.c5405f9d.jpg"
+  },
+  {
+    "revision": "2110fd853d39fe62733152ac2bc13cd1",
+    "url": "/ecommerce/static/media/cross-bracelet_720x480.2110fd85.jpg"
+  },
+  {
+    "revision": "a89f265cdd3ca88f1515217f569dd2b4",
+    "url": "/ecommerce/static/media/crystal-pendent_720x480.a89f265c.jpg"
+  },
+  {
+    "revision": "5ae6d08c0993c32392cfd7d58662cf67",
+    "url": "/ecommerce/static/media/dolphin-ring_720x480.5ae6d08c.jpg"
+  },
+  {
+    "revision": "d5ef465a7ca0b0d50dccc34e8799af50",
+    "url": "/ecommerce/static/media/elegant-pendent_720x480.d5ef465a.jpg"
+  },
+  {
+    "revision": "3ea73e44f29162549a34cde87fedd2de",
+    "url": "/ecommerce/static/media/golden-ring_720x480.3ea73e44.jpg"
+  },
+  {
+    "revision": "2d3f281b62c81714333a66000e613352",
+    "url": "/ecommerce/static/media/handmade-necklace_720x480.2d3f281b.jpg"
+  },
+  {
+    "revision": "d37cc48c717cfad6a3af3ca8bc824803",
+    "url": "/ecommerce/static/media/heart-shaped-pendent_720x480.d37cc48c.jpg"
+  },
+  {
+    "revision": "1e1ede27b01f2cfae6f5a40307432bae",
+    "url": "/ecommerce/static/media/modern-earing_720x480.1e1ede27.jpg"
+  },
+  {
+    "revision": "4eab7aa76537c3bbebf99a2c0fc0b064",
+    "url": "/ecommerce/static/media/pigeon-pendent_720x480.4eab7aa7.jpg"
+  },
+  {
+    "revision": "ad3668b4125f7cfbec4d8bd90868bf63",
+    "url": "/ecommerce/static/media/review-one_60x60.ad3668b4.jpg"
+  },
+  {
+    "revision": "30b2f073579f1af17fdc798eb7380598",
+    "url": "/ecommerce/static/media/review-three_60x60.30b2f073.jpg"
+  },
+  {
+    "revision": "29531747cde81a9fc303767484a4bc06",
+    "url": "/ecommerce/static/media/review-two_60x60.29531747.jpg"
+  },
+  {
+    "revision": "b41fc97b0f44c812533aa4bbc51c7e0c",
+    "url": "/ecommerce/static/media/ring-band_720x480.b41fc97b.jpg"
+  },
+  {
+    "revision": "58cd10adee28663201a5da6be09d3147",
+    "url": "/ecommerce/static/media/sea-turtle-ring_720x480.58cd10ad.jpg"
+  },
+  {
+    "revision": "f629c0fe893feeefdf5bfce34af33189",
+    "url": "/ecommerce/static/media/silver-band_720x480.f629c0fe.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ecommerce/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ecommerce/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ecommerce/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ecommerce/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "da9bf6adbc01160423e589f56840c4eb",
+    "url": "/ecommerce/static/media/stack-ring_720x480.da9bf6ad.jpg"
+  },
+  {
+    "revision": "42114f4ba8a20b1c2e00e293b6f47244",
+    "url": "/ecommerce/static/media/streamlined-earing_720x480.42114f4b.jpg"
+  },
+  {
+    "revision": "cafb0019f58ffee66483a698512ae6f4",
+    "url": "/ecommerce/static/media/tassel-earing_720x480.cafb0019.jpg"
+  },
+  {
+    "revision": "30afb12b5560b5bbdbbce263cfca19f2",
+    "url": "/ecommerce/static/media/victorian-earing_720x480.30afb12b.jpg"
+  }
+]);
